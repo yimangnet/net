1cfad4dc8f719788601e551ad1c15a81346225ab
yimangnet <yimangnet@gmail.com>
2023-02-05 02:56:08 +0800

    add login page

--- script.js
+++ script.js
@@ -13,15 +13,20 @@ function parseCookie() {
 }
 
 
-function getCookie(name) {
-    var value = parseCookie()[name];
-    if (value) {
-        value = decodeURIComponent(value);
-    }
-
-    return value;
+function getCookie (name)
+{
+	if (document.cookie.length > 0) { // if there are any cookies
+		var search = name + "=";
+		offset = document.cookie.indexOf(search);
+		if (offset != -1) { // if cookie exists
+			offset += search.length;
+			// set index of beginning of value
+			end = document.cookie.indexOf(";", offset);
+			// set index of end of cookie value
+			if (end == -1)
+				end = document.cookie.length;
+			return unescape(document.cookie.substring(offset, end));
+		}
+	}
+	return null;
 }
-var username = getCookie('username');
-document.write(username)
-
-

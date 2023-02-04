6cc037d1ab90f5f9e995c300584b2fbb08cb4081
yimangnet <yimangnet@gmail.com>
2023-02-05 03:45:50 +0800

    add logout page

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-function parseCookie() {
-    var cookieObj = {};
-    var cookieAry = document.cookie.split(';');
-    var cookie;
-    
-    for (var i=0, l=cookieAry.length; i<l; ++i) {
-        cookie = jQuery.trim(cookieAry[i]);
-        cookie = cookie.split('=');
-        cookieObj[cookie[0]] = cookie[1];
+function ShowMeDate() {
+    　var Today=new Date();
+    　alert("今天日期是 " + Today.getFullYear()+ " 年 " + (Today.getMonth()+1) + " 月 " + Today.getDate() + " 日");
     }
     
-    return cookieObj;
-}
-
-
-function getCookie (name)
-{
-	if (document.cookie.length > 0) { // if there are any cookies
-		var search = name + "=";
-		offset = document.cookie.indexOf(search);
-		if (offset != -1) { // if cookie exists
-			offset += search.length;
-			// set index of beginning of value
-			end = document.cookie.indexOf(";", offset);
-			// set index of end of cookie value
-			if (end == -1)
-				end = document.cookie.length;
-			return unescape(document.cookie.substring(offset, end));
-		}
-	}
-	return null;
-}
+<button onclick="ShowMeDate()">告訴我今天日期</button>

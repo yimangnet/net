801c4bcf0e3e6cec05d79adb7cef14f7f0b59e22
yimangnet <yimangnet@gmail.com>
2023-02-05 02:27:32 +0800

    cookie test

---
+++ script.js
@@ -0,0 +1,27 @@
+function parseCookie() {
+    var cookieObj = {};
+    var cookieAry = document.cookie.split(';');
+    var cookie;
+    
+    for (var i=0, l=cookieAry.length; i<l; ++i) {
+        cookie = jQuery.trim(cookieAry[i]);
+        cookie = cookie.split('=');
+        cookieObj[cookie[0]] = cookie[1];
+    }
+    
+    return cookieObj;
+}
+
+
+function getcookie(name) {
+    var value = parseCookie()[name];
+    if (value) {
+        value = decodeURIComponent(value);
+    }
+
+    return value;
+}
+var username = getcookie('username');
+document.write(username)
+
+
